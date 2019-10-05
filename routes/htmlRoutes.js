44b1abd17d385f24ea3eedddc26075b546321050
Alex Shoemaker <iamalexshoemaker@gmail.com>
2019-10-05 16:54:38 -0700

    got news and blog posts rendering

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -18,17 +18,17 @@ module.exports = function (app) {
       });
   });
 
-  app.get("/", function (req, res) {
-    db.Post.findAll({
-      limit: 5,
-      order: [['createdAt', 'DESC']],
-    })
-      .then(function (results) {
-        res.render("", {
-          blog: results
-        });
-      });
-  });
+  // app.get("/", function (req, res) {
+  //   db.Post.findAll({
+  //     limit: 5,
+  //     order: [['createdAt', 'DESC']],
+  //   })
+  //     .then(function (results) {
+  //       res.render("", {
+  //         blog: results
+  //       });
+  //     });
+  // });
 
   // Load example page and pass in a post by id
   app.get("/blog-post/:id", function (req, res) {
@@ -48,61 +48,58 @@ module.exports = function (app) {
   //Set Handlebar routes 
   app.get("/", function (req, res) {
     callHome(function (doneHome) {
-      res.render("home", {
-        home: doneHome
-      });
-    });
-    db.Post.findAll({
-      limit: 5,
-      order: [['createdAt', 'DESC']],
-    })
-      .then(function (results) {
-        res.render("home", {
-          blog: results
+      db.Post.findAll({
+        limit: 5,
+        order: [['createdAt', 'DESC']]
+      })
+        .then(function (results) {
+          res.render("home", {
+            blog: results,
+            home: doneHome
+          });
         });
-      });
-
-  });
-
-  app.get("/technology", function (req, res) {
-    callTech(function (doneTech) {
-      res.render("technology", {
-        tech: doneTech
-      });
     });
   });
 
-  app.get("/business", function (req, res) {
-    callBus(function (doneBus) {
-      res.render("business", {
-        bus: doneBus
-      });
+app.get("/technology", function (req, res) {
+  callTech(function (doneTech) {
+    res.render("technology", {
+      tech: doneTech
     });
   });
+});
 
-  app.get("/sports", function (req, res) {
-    callSports(function (doneSports) {
-      res.render("sports", {
-        sports: doneSports
-      });
+app.get("/business", function (req, res) {
+  callBus(function (doneBus) {
+    res.render("business", {
+      bus: doneBus
     });
   });
+});
 
-  app.get("/entertainment", function (req, res) {
-    callEnt(function (doneEnt) {
-      res.render("entertainment", {
-        ent: doneEnt
-      });
+app.get("/sports", function (req, res) {
+  callSports(function (doneSports) {
+    res.render("sports", {
+      sports: doneSports
     });
   });
+});
 
-  app.get("/health", function (req, res) {
-    callHealth(function (doneHealth) {
-      res.render("health", {
-        health: doneHealth
-      });
+app.get("/entertainment", function (req, res) {
+  callEnt(function (doneEnt) {
+    res.render("entertainment", {
+      ent: doneEnt
     });
   });
+});
+
+app.get("/health", function (req, res) {
+  callHealth(function (doneHealth) {
+    res.render("health", {
+      health: doneHealth
+    });
+  });
+});
 
 //   app.get("/blog", function (req, res) {
 //     res.render("blog", {
